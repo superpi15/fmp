fff7f9f90268ffe07e300a4a26bf0ffdfaea8d4c
deveth <superpi152@gmail.com>
2018-04-08 14:59:13 +0800

    Changes to be committed: 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -26,12 +27,22 @@ public:
 	void cpush_front( Cell_t * pCell ){
 		assert( is_root() ); 
 		assert( pCell->prev == NULL && pCell->next == NULL ); // check for unhook
-		pCell->next = next;
-		pCell->prev = this;
-		if( next ){
-			next->prev = pCell;
+		Cell_t * pHead = this;/**
+		for( ; pHead->next; pHead = pHead->next ){
+			if( pHead == this ) 
+				continue;
+			if( pHead->next ){
+				if( pHead->next->size() <= pCell->size() )
+					break;
+			}
 		}
-		next = pCell;
+		/**/
+		pCell->next = pHead->next;
+		pCell->prev = pHead;
+		if( pHead->next ){
+			pHead->next->prev = pCell;
+		}
+		pHead->next = pCell;
 	}
 	void cunhook(){
 		assert( !is_root() );
@@ -53,6 +64,11 @@ public:
 		assert( is_root() );
 		return next == NULL;
 	}
+	struct Cmptor_Size_Ptr{
+		bool operator()( Cell_t* pA, Cell_t* pB ) const {
+			return pA->size()>pB->size();
+		}
+	};
 };
 
 double ParseFile( char * ifname, std::map<std::string,int>& NetMap
@@ -61,18 +77,56 @@ void Update_Gain( int CellID, std::map<int,Cell_t>* , std::vector<Net_t>&, std::
 
 struct Cmptor{
 	bool operator()( const int& a, const int& b ) const {
-		return a>b;
+		return a<b;
 	}
 };
 
-std::map<int,Cell_t,Cmptor>::iterator get_nontrivial( std::map<int,Cell_t,Cmptor>& Map ){
-	while( !Map.empty() ){
-		if( Map.begin()->second.cempty() )
-			Map.erase( Map.begin() );
-		else
-			return Map.begin();
+void bucket_sort( Cell_t& Root ){
+	size_t size = Root.csize();
+	if( size == 0 )
+		return;
+	std::vector<Cell_t*> tmp( size );
+	int i=0;
+	for( Cell_t * cur = Root.next; cur != NULL; cur=cur->next, i++ ){
+		tmp[i] = cur;
 	}
-	return Map.end();
+	std::sort( tmp.begin(), tmp.end(), Cell_t::Cmptor_Size_Ptr() );
+	Cell_t * prev = &Root;	
+	Root.next = tmp[0];
+	for( i=0; i<tmp.size(); i++ ){
+		tmp[i]->prev = prev;
+		tmp[i]->next = (i==tmp.size()-1)? NULL: tmp[i+1];
+		prev = tmp[i];
+	}
+}
+
+typedef std::map<int,Cell_t,Cmptor> bucket_t;
+bucket_t::reverse_iterator get_nontrivial( std::map<int,Cell_t,Cmptor>& Map ){
+	while( !Map.empty() ){
+		if( Map.rbegin()->second.cempty() )
+			Map.erase( Map.rbegin()->first );
+		else {
+			//bucket_sort( Map.begin()->second );
+			return Map.rbegin();
+		}
+	}
+	return Map.rend();
+}
+
+void dump_bucket( bucket_t& bucket ){
+	bucket_t::reverse_iterator itr;
+	for( itr = bucket.rbegin(); itr != bucket.rend(); itr ++ ){
+		Cell_t * pC;
+		if( itr->second.csize() == 0 )
+			continue;
+		std::cout<<itr->first<<": ";
+		if( itr->second.next == NULL )
+			continue;
+		for( pC = itr->second.next; pC != NULL; pC =pC->next ){
+			std::cout<< pC->Name <<" ";
+		}
+		std::cout<<std::endl;
+	}
 }
 
 void Apply_Change( int MinStep, std::vector<Cell_t>& vCell, std::vector<bool>& vInitState, std::vector<int>& vStep ){
@@ -104,6 +158,11 @@ int CountCut( std::vector<Net_t>& vNet, std::vector<Cell_t>& vCell ){
 	}
 	return LocalMinCut;
 }
+
+bool is_leagal( int lb, int ub, int c ){
+	return lb < c && c < ub;
+}
+
 
 int FM( double ratio, std::vector<Net_t>& vNet, std::vector<Cell_t>& vCell ){
 	// Balance placement
@@ -147,7 +206,7 @@ int FM( double ratio, std::vector<Net_t>& vNet, std::vector<Cell_t>& vCell ){
 	
 	// Pu object onto Bucket
 	std::map<int,Cell_t,Cmptor> Bucket[2];
-	std::map<int,Cell_t,Cmptor>::iterator itr, itrs[2];
+	std::map<int,Cell_t,Cmptor>::reverse_iterator itr, itrs[2];
 	for( int i=0; i<vCell.size(); i++ ){
 		Bucket[ vCell[i].region ][ vCell[i].gain ].cpush_front( &vCell[i] );
 	}
@@ -160,17 +219,53 @@ int FM( double ratio, std::vector<Net_t>& vNet, std::vector<Cell_t>& vCell ){
 	std::cout<<"vCell.size()= "<< vCell.size() <<std::endl;
 	*/
 	std::cout<<"Init cut="<< nCut<< std::endl;
-	int Bound[2];
+	int Bound[2], imba_block = 0, cforce_balance = 0;
 	int MinCut = nCut;
+	bool Once_leagal = is_leagal(Bound[0],Bound[1],cRegion[0]) && is_leagal(Bound[0],Bound[1],cRegion[1]);
 	Bound[0] = int((double) 0.5*(1.0-ratio)*vCell.size());
 	Bound[1] = int((double) 0.5*(1.0+ratio)*vCell.size());
 	for( int i=0; i<vCell.size(); i++ ){
 		itrs[0] = get_nontrivial( Bucket[0] );
 		itrs[1] = get_nontrivial( Bucket[1] );
-		itr = (itrs[0]!=Bucket[0].end() && itrs[1]!=Bucket[1].end())
-			? ( ( itrs[0]->first > itrs[1]->first )? itrs[0]: itrs[1] )
-			: ( itrs[0] != Bucket[0].end()? itrs[0]: itrs[1] );
-		assert( itr != Bucket[0].end() && itr != Bucket[1].end() );
+		itr = (itrs[0]!=Bucket[0].rend() && itrs[1]!=Bucket[1].rend())
+			? ( ( itrs[0]->first >= itrs[1]->first )? itrs[0]: itrs[1] )
+			: ( itrs[0] != Bucket[0].rend()? itrs[0]: itrs[1] );
+		/**/
+		bool force_balance = false;
+		/**/
+		if( Bound[0] > (cRegion[0]-1 ) && itrs[1] != Bucket[1].rend() ){
+			//printf("1: %d %d %d %d %d\n"\
+				,  Bound[0] > (cRegion[0]-1 ) , (cRegion[1]+1) > Bound[1] , itrs[1] != Bucket[1].end(), Bucket[0].size(), Bucket[1].size()  );
+			itr = itrs[1];
+			//std::cout<<"1"<<std::endl;
+			force_balance = true;
+
+		} else
+		if( Bound[0] > (cRegion[1]-1 ) && itrs[0] != Bucket[0].rend() ){
+			//printf("0: %d %d %d %d %d\n"\
+				,  Bound[0] > (cRegion[1]-1 ) , (cRegion[0]+1) > Bound[1] , itrs[0] != Bucket[0].end(), Bucket[0].size(), Bucket[1].size()  );
+			itr = itrs[0];
+			//std::cout<<"0"<<std::endl;
+			force_balance = true;
+		} else
+		if( (cRegion[1]+1) > Bound[1]  && itrs[1] != Bucket[1].rend() ){
+			//printf("1: %d %d %d %d %d\n"\
+				,  Bound[0] > (cRegion[0]-1 ) , (cRegion[1]+1) > Bound[1] , itrs[1] != Bucket[1].end(), Bucket[0].size(), Bucket[1].size()  );
+			itr = itrs[1];
+			//std::cout<<"1"<<std::endl;
+			force_balance = true;
+		} else
+		if( (cRegion[0]+1) > Bound[1]  && itrs[0] != Bucket[0].rend() ){
+			//printf("0: %d %d %d %d %d\n"\
+				,  Bound[0] > (cRegion[1]-1 ) , (cRegion[0]+1) > Bound[1] , itrs[0] != Bucket[0].end(), Bucket[0].size(), Bucket[1].size()  );
+			itr = itrs[0];
+			//std::cout<<"0"<<std::endl;
+			force_balance = true;
+		}
+	
+
+		/**/
+		assert( itr != Bucket[0].rend() && itr != Bucket[1].rend() );
 		Cell_t& Cell = *itr->second.next;
 		int cRegionNext[2];
 		cRegionNext[0] = cRegion[0] + ( Cell.region==0? -1: +1 );
@@ -178,26 +273,59 @@ int FM( double ratio, std::vector<Net_t>& vNet, std::vector<Cell_t>& vCell ){
 		//std::cout<< Bound[0]<<":"<<Bound[1]<<std::endl;
 		//std::cout<< cRegionNext[0] <<":"<< cRegionNext[1]<<std::endl;
 		//std::cout<< cRegion[0] <<":"<< cRegion[1]<<std::endl;
-		if( Bound[0] <= (cRegionNext[0] ) && cRegionNext[0] <= Bound[1] 
-		&& Bound[0] <= (cRegionNext[1] ) && cRegionNext[1] <= Bound[1] ){
+		/**
+		nCut -= itr->first;
+		//std::cout<<"step "<<cStep<<", cut "<< nCut<<":"<< itr->first<<std::endl;
+		if( force_balance 
+		|| ( nCut <= MinCut && is_leagal( Bound[0], Bound[1], cRegionNext[0] ) &&  is_leagal( Bound[0], Bound[1], cRegionNext[1]) )
+		|| ( nCut==MinCut && abs( cRegionNext[0]- cRegionNext[1] )< abs( cRegion[0]- cRegion[1] ) ) 
+		){
+			MinCut = nCut;
+			MinStep = cStep+1;
+		}
+		int CellID = &Cell- vCell.data();
+		Update_Gain( CellID, (std::map<int,Cell_t>*) Bucket, vNet, vCell );
+		//assert( CountCut( vNet, vCell)==nCut );
+		vStep[ cStep++ ] = CellID;
+		cRegion[0] = cRegionNext[0];
+		cRegion[1] = cRegionNext[1];
+		/**/
+		/**
+		std::cout<< "*********0*********" <<std::endl;
+		dump_bucket( Bucket[0] );
+		std::cout<< "*********1*********" <<std::endl;
+		dump_bucket( Bucket[1] );
+		**
+		std::cout<<"Choose = "<< (itrs[0]!=Bucket[0].rend()? itrs[0]->first:-77) <<":"
+		<< (itrs[1]!=Bucket[1].rend()? itrs[1]->first:-77) <<":"<< itr->first<<std::endl;
+		/**/
+		if( force_balance || 
+		( Bound[0] <= (cRegionNext[0] ) && cRegionNext[0] <= Bound[1] 
+		&& Bound[0] <= (cRegionNext[1] ) && cRegionNext[1] <= Bound[1] ) ){
 			nCut -= itr->first;
-			//std::cout<< nCut<<std::endl;
+			Once_leagal |= is_leagal(Bound[0],Bound[1],cRegion[0]) && is_leagal(Bound[0],Bound[1],cRegion[0]);
+			//std::cout<<"step "<<cStep<<", cut "<< nCut<<":"<< itr->first<<std::endl;
 			if( nCut <= MinCut 
-			|| ( nCut==MinCut && abs( cRegionNext[0]- cRegionNext[1] )< abs( cRegion[0]- cRegion[1] ) ) ){
+			|| ( nCut==MinCut && abs( cRegionNext[0]- cRegionNext[1] )< abs( cRegion[0]- cRegion[1] ) ) 
+			|| ( (!Once_leagal) && force_balance ) ){
 				MinCut = nCut;
 				MinStep = cStep+1;
 			}
 			int CellID = &Cell- vCell.data();
+			//std::cout<< Cell.Name <<":"<< itr->first <<std::endl<<std::endl;
 			Update_Gain( CellID, (std::map<int,Cell_t>*) Bucket, vNet, vCell );
+			//assert( CountCut( vNet, vCell)==nCut );
 			vStep[ cStep++ ] = CellID;
 			cRegion[0] = cRegionNext[0];
 			cRegion[1] = cRegionNext[1];
 		} else {
+			imba_block ++;
 			Cell.cunhook();
 			Cell.Lock = true;
 		}
+		/**/
 	}
-	std::cout<<"MinCut="<<MinCut<<", cStep="<<cStep<<std::endl;
+	std::cout<<"MinCut="<<MinCut<<", MinStep="<< MinStep<<", cStep="<<cStep<<", imba_block="<<imba_block<<std::endl;
 	Apply_Change( MinStep, vCell, vInitState, vStep );
 	return MinCut;
 }
@@ -226,18 +354,63 @@ int main( int argc, char * argv[] ){
 	/**/
 	size_t half_size = vCell.size()/2;
 	//initial
-	for( int i=0; i<half_size; i++ )
+	/**/
+	//std::cout<<"1"<<std::endl;
+	for( int i=0; i<half_size; i++ ){
+		//std::cout<< vCell[i].Name << " ";
 		vCell[i].region = 0;
-	for( int i=half_size; i<vCell.size(); i++ )
-		vCell[i].region = 1;
+	}
+	//std::cout<<std::endl;
+	//std::cout<<"1"<<std::endl;
+	for( int i=half_size; i<vCell.size(); i++ ){
+		//std::cout<< vCell[i].Name << " ";
+		vCell[i].region = 0;
+	}
+	//std::cout<<std::endl;
+	/**
+	for( int i=0; i<vCell.size(); i++ ){
+		vCell[i].region = 0;
+		vCell[i].Lock = false;
+	}
+	size_t cG2 = 0;
+	for( int i=0; i<vNet.size(); i++ ){
+		Net_t& Net = vNet[i];
+		for( int j=0; j<Net.size(); j++ ){
+			Cell_t& Cell = vCell[Net[j]];
+			if( Cell.Lock )
+				continue;
+			if( cG2 == half_size )
+				break;
+			cG2 ++;
+			Cell.Lock = true;
+			Cell.region = 1;
+		}
+	}
+	for( int i=0; i<vCell.size(); i++ ){
+		vCell[i].Lock = false;
+	}
+	/**/
 
 	int InitCut = CountCut( vNet, vCell );
 	int MinCut = INT_MAX, CurCut, Iter = 0;
+	int Escape = 10;
 	bool Continue = true;
 	do {
 		CurCut = FM( ratio, vNet, vCell );
+		/**/
 		if( CurCut >= MinCut )
 			Continue = false;
+		/**
+		if( CurCut >= MinCut ){
+			if( Escape>0 )
+				Escape --;
+			else
+				Continue = false;
+		} else {
+			if( Escape <= 10)
+				Escape ++;
+		}**/
+
 		MinCut = ( CurCut < MinCut )? CurCut: MinCut;
 		printf("Iteration %4d , Cut = %10d (min= %10d)\n", Iter, CurCut, MinCut );
 		Iter++;
